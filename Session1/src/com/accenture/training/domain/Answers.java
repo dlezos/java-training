ff74e0dd064bbbf10e80224a6ec0c01fba07c5ae
marileou <maria.leousi@gmail.com>
2016-11-07 11:44:23 +0200

    m

---
+++ Session1/src/com/accenture/training/domain/Answers.java
@@ -0,0 +1,47 @@
+package com.accenture.training.domain;
+
+public class Answers {
+	public String AnswerA="";
+	public String AnswerB="";
+	public String AnswerC="";
+	public String AnswerD="";
+
+	
+	
+	public String getAnswerA( String AnswerA) {
+		return AnswerA;
+	}
+
+	public void setAnswerA(String AnswerA) {
+		this.AnswerA = AnswerA;
+	}
+	
+	public String getAnswerB( String AnswerB) {
+		return AnswerB;
+	}
+
+	public void setAnswerB(String AnswerB) {
+		this.AnswerB = AnswerB;
+	}
+	public String getAnswerC( String AnswerC) {
+		return AnswerC;
+	}
+
+	public void setAnswerC(String AnswerC) {
+		this.AnswerC = AnswerC;
+	}
+	public String getAnswerD( String AnswerD) {
+		return AnswerD;
+	}
+
+	public void setAnswerD(String AnswerD) {
+		this.AnswerD = AnswerD;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+}
+
