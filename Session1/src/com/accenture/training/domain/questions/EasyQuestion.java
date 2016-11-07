33bdb3a66148574fb63a73a59373fc181eecb806
jathanasiou <john.athanasiou1@gmail.com>
2016-11-07 11:34:04 +0200

    added initial Question implementations

---
+++ Session1/src/com/accenture/training/domain/questions/EasyQuestion.java
@@ -0,0 +1,7 @@
+package com.accenture.training.domain.questions;
+import com.accenture.training.interfaces.Question;
+import com.accenture.training.domain.questions.QuestionAbstr;
+
+public class EasyQuestion extends QuestionAbstr{
+
+}
